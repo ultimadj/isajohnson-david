47c57fa15b1fbd24cf64790a0ce763136b500d08
David Johnson <ultimadj@gmail.com>
2018-04-22 08:16:36 -0400

    initial tweaks

---
+++ src/pages/page-3.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const ThirdPage = () => (
+  <div>
+    <h1>Hi from the third page</h1>
+    <h4>Fourth heading</h4>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default ThirdPage
